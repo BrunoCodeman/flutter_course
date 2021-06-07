ffafb9e5b8723518bfebf84d10486d102dc555de
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-06-06 22:41:02 -0300

    conditional rendering widgets

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,15 @@ class _MyAppState extends State<MyApp> {
       "questionText": "What's your favorite animal?",
       "answers": ["Cat", "Dog", "Capybara"]
     },
+    {
+      "questionText": "What's your drink?",
+      "answers": ["Juice", "Wine", "Milk"]
+    }
   ];
   var _questionIndex = 0;
   void _addAnswer() {
     setState(() {
-      _questionIndex = _questionIndex == 0 ? 1 : 0;
+      _questionIndex += 1;
     });
   }
 
@@ -38,14 +42,22 @@ class _MyAppState extends State<MyApp> {
           title: Text("My App"),
         ),
         body: Container(
-          child: Column(children: [
-            Question(questions[_questionIndex]["questionText"] as String),
-            // ... funciona como em JS: separa a lista em items individuais
-            ...(questions[_questionIndex]["answers"] as List<String>)
-                .map((answer) {
-              return Answer(_addAnswer, answer);
-            }).toList()
-          ]),
+          child: _questionIndex >= questions.length
+              ? Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text("No more questions",
+                      style: TextStyle(
+                        fontSize: 16,
+                      )),
+                )
+              : Column(children: [
+                  Question(questions[_questionIndex]["questionText"] as String),
+                  // ... funciona como em JS: separa a lista em items individuais
+                  ...(questions[_questionIndex]["answers"] as List<String>)
+                      .map((answer) {
+                    return Answer(_addAnswer, answer);
+                  }).toList()
+                ]),
         ),
       ),
     );
